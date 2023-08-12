43431a073287421aaa0f978237baa901cfb7c222
RogerioCrizan <crizanrogerio@gmail.com>
2023-08-12 02:19:09 -0300

    create shelf

--- src/components/Shelf/components/Product.tsx
+++ src/components/Shelf/components/Product.tsx
@@ -3,10 +3,16 @@ import type { IProduct } from "../typings/products";
 function Product({ image, title, rating, price, id }: IProduct) {
   return (
     <div className="shelf__product">
-      <img src={image} alt="" />
+      <div className="shelf__product-image">
+        <img src={image} alt="" />
+      </div>
       <div className="shelf__product-info">
-        <p>{title}</p>
-        <p>{price}</p>
+        <div className="shelf__product-nameAndRating">
+          <p className="shelf__product-name">{title}</p>
+        </div>
+        <p className="shelf__product-price">
+          por R${price.toFixed(2).replace(".", ",")}
+        </p>
       </div>
     </div>
   );
--- src/components/Shelf/index.tsx
+++ src/components/Shelf/index.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import Product from "./components/Product";
 import { IProduct } from "./typings/products";
+import Glider from "react-glider";
+import "glider-js/glider.min.css";
 
 import "./styles/shelf.scss";
 
@@ -21,17 +23,26 @@ function Shelf() {
   return (
     <section className="shelf">
       <div className="shelf__wrapper">
-        {products.map((item: IProduct) => {
-          return (
-            <Product
-              image={item.image}
-              title={item.title}
-              rating={item.rating}
-              price={item.price}
-              id={item.id}
-            />
-          );
-        })}
+        <h2>Mais Vendidos</h2>
+        <Glider
+          draggable
+          hasArrows
+          slidesToShow={4}
+          slidesToScroll={1}
+          itemWidth={216}
+        >
+          {products.map((item: IProduct) => {
+            return (
+              <Product
+                image={item.image}
+                title={item.title}
+                rating={item.rating}
+                price={item.price}
+                id={item.id}
+              />
+            );
+          })}
+        </Glider>
       </div>
     </section>
   );
